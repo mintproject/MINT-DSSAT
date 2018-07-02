4ee658ece10084471f231cf0517a6fc1655ff6a9
Mats Rynge <rynge@isi.edu>
2018-07-02 14:17:37 -0700

    Fixed DSSAT setup

--- DSSAT/DSSAT-wrapper.sh
+++ DSSAT/DSSAT-wrapper.sh
@@ -5,20 +5,23 @@ set -e
 chmod 755 DSSAT-data-find
 #./DSSAT-data-find
 wget -nv http://workflow.isi.edu/MINT/DSSAT/ss_example_sparse.tar.gz
-
 tar xzf ss_example_sparse.tar.gz
+mv ss_example_sparse DSSAT-state
 
-cd ss_example_sparse/
+#wget -nv http://workflow.isi.edu/MINT/DSSAT/hsc.tar.gz
+#tar xzf hsc.tar.gz
+#mv hsc DSSAT-state
+
+cd DSSAT-state
 
 # run DSSAT
-export PATH=/opt/dssat:$PATH
 echo
 echo
-dssat A MINK99RM.SNX 2>&1
+/opt/dssat/dscsm047 A MINK99RM.SNX 2>&1
+#/opt/dssat/dscsm047 A ARMA0701.WHX 2>&1
 echo
 echo
 
 cd ..
-mv ss_example_sparse DSSAT-state
 tar czf DSSAT-state.tar.gz DSSAT-state
 
